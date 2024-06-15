2a1818a7678409bb9f8286a52180b5a82534a4f4
ChetSovisoth <chet.sovisoth@gmail.com>
2024-06-15 15:30:27 +0700

    retriction on student

--- resources/views/group/group_detail.blade.php
+++ resources/views/group/group_detail.blade.php
@@ -6,7 +6,7 @@
         <div class="mt-4">
             <h1>Group Members:</h1>
             <div class="row">
-                <div class="col-md-4 mb-4">
+                <div class="col-md-4 mb-4 w-75">
                     <table class="table table-striped">
                         <thead>
                             <tr class="text-center">
@@ -16,15 +16,25 @@
                         </thead>
                         <tbody>
                             @foreach($members as $member)
-                            <tr class="text-center">
-                                <td>{{ (new App\Models\User())::find($member->followable->id)->name }}</td>
-                                <td>{{ (new App\Models\User())::find($member->followable->id)->email }}</td>
-                            </tr>
+                                <tr class="text-center">
+                                    @php
+                                        $user = App\Models\User::find($member->followable->id);
+                                    @endphp
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
+        </div>
+        <div class="align-self-center ms-4">
+            <form action="{{ route('group.delete', ['name' => $name, 'uuid' => $uuid]) }}" method="POST" class="m-0">
+                @csrf
+                @method('delete')
+                <button class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this group?')">Delete Group</button>
+            </form>
         </div>
     </div> 
 </div>
--- resources/views/livewire/group/group-general.blade.php
+++ resources/views/livewire/group/group-general.blade.php
@@ -1,21 +1,24 @@
 <div class="card mt-4">
     <div class="card-header w-100">
-        <a href="{{ route('group.detail', ['name' => $name, 'uuid' => $uuid]) }}" class="text-decoration-none text-black d-flex">
-            <div class="flex-grow-1">
-                <img src="{{ (new App\Models\Group())->getGroupPhotoByPhoto($photo) }}" alt="profile-picture" class="rounded-circle mx-3 " style="width: 50px; height: 50px; object-fit: cover;">
+        @if(auth()->user()->role === 'admin' || auth()->user()->role === 'mentor')
+            <a href="{{ route('group.detail', ['name' => $name, 'uuid' => $uuid]) }}" class="text-decoration-none text-black d-flex">
+                <div class="flex-grow-1">
+                    <img src="{{ (new App\Models\Group())->getGroupPhotoByPhoto($photo) }}" alt="profile-picture" class="rounded-circle mx-3 " style="width: 50px; height: 50px; object-fit: cover;">
+                </div>
+                <div class="align-self-center">
+                    {{ $name }}
+                </div>
+            </a>
+        @else
+            <div class="d-flex">
+                <div class="flex-grow-1">
+                    <img src="{{ (new App\Models\Group())->getGroupPhotoByPhoto($photo) }}" alt="profile-picture" class="rounded-circle mx-3 " style="width: 50px; height: 50px; object-fit: cover;">
+                </div>
+                <div class="align-self-center">
+                    {{ $name }}
+                </div>
             </div>
-            <div class="align-self-center">
-                {{ $name }}
-            </div>
-
-            <div class="align-self-center ms-4">
-                <form action="{{ route('group.delete', ['name' => $name, 'uuid' => $uuid]) }}" method="POST" class="m-0">
-                    @csrf
-                    @method('delete')
-                    <button class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this group?')">Delete</button>
-                </form>
-            </div>
-        </a>
+        @endif
     </div>
     <div class="card-body">
         <h5 class="card-title">Group Code: {{ $code }}</h5>
